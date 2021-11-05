35d27fdd60e45506ee40b079ea40031e5a968043
Kartik <ksihotiya@gmail.com>
2021-11-05 14:36:50 +0530

    added server1.js and jwt code working  properly

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const express = require("express");
+const jwt = require("jsonwebtoken")
 const app = express();
-
+require("dotenv").config();
+app.use(express.json());
 const posts = [
     {
-        username:'Kartik',
-        title:'post 1'
+        username:"Kartik",
+        title:"post 1"
     },
     {
-        username:'Jonh',
-        title:'post 2'
+        username:"John",
+        title:"post 2"
     }
 ]
 
-app.get('/posts',(req,res) => {
-res.json(posts);
+app.get('/posts', authenticateToken, (req,res) => {
+    jwt.verify(req.token,process.env.token_key,(err,data) => {
+        
+        if(err){res.sendStatus(403)}
+        else{
+            console.log(data)
+            res.json(posts.filter(post => post.username === data.user.name));
+        }
+    })
+    
 })
 
-app.listen(8000)
+app.post('/login',(req,res)=> {
+    const username = req.body.username
+    const user = {name:username}
+    //serializing user with secret key present in .env file
+    jwt.sign({user},process.env.token_key,(err,token) => {
+        res.json({token});
+    });
+    
+});
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    
+    if(token == null) return res.sendStatus(401)
+    
+    req.token = token;
+    next();
+    
+}
+
+app.listen(8000, ()=> {
+    console.log("server is  running");
+})
